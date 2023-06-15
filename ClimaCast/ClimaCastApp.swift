6a05bd3ecba205f67247196dd6ccdef98c35e96e
Negarnbkhti42 <norob35@gmail.com>
2023-06-15 19:59:20 +0330

    init project

---
+++ ClimaCast/ClimaCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaCastApp.swift
+//  ClimaCast
+//
+//  Created by Saee Saadat on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ClimaCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
