75b6cf50730aa23b8471136daf1f99886e46cf52
Negarnbkhti42 <norob35@gmail.com>
2023-07-01 10:36:30 +0330

    Add result fetch

--- ClimaCast/SearchPage.swift
+++ ClimaCast/SearchPage.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct SearchPage: View {
+    @Environment(\.presentationMode) var presentationMode
+
     @Binding var items: [String]
     @State var SearchVlue = "";
+    @state var presentAlert = false;
+
+    func search() async {
+        do {
+            let result = try await WeatherService().search(query: SearchVlue)
+            print(result)
+            if result != nil {
+                items.append(result!)
+                presentationMode.wrappedValue.dismiss()
+            } else {
+                presentAlert = true
+            }
+        } catch {
+            print(error)
+        }
+    }
+
     var body: some View {
         NavigationView{
             Form{
                 TextField("FavoriteLocation" ,text: $SearchVlue)
             }
             Button("Cancel" , role: .cancel){}
+
+            .alert("no result found!", isPresented: $presentAlert){
+                Button("OK", role: .cancel){}
+            }
         }
         .navigationTitle(Text("Add new Favorite Location"))
         .toolbar{
             Button{
-                
+                await search()
             }label:{
                 Text("Add")
             }
