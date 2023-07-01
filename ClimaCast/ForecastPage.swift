1e5bb62898cc21038e8d8ee1b80a534efe26eeb8
AinazRafiei <ainazrsh2016@gmail.com>
2023-07-01 13:36:50 +0330

    fix indents

--- ClimaCast/ForecastPage.swift
+++ ClimaCast/ForecastPage.swift
@@ -1,52 +1,52 @@
 import SwiftUI
 
 struct ForecastPage: View {
-     var location  : String
+    var location  : String
     @State var foreCast : ForecastResponse? = nil
-//    @State var foreCast: ForecastResponse = ForecastResponse(
-//        location: LocationParams(
-//            name: "London",
-//            localtime: "2023-07-01"
-//        ),
-//        forecast: ForecastParams(
-//            forecastday: [
-//                Forecast(
-//                    date: "2023-07-01",
-//                    day: Day(
-//                        avgtemp_c: 10,
-//                        avgtemp_f: 50,
-//                        condition: conditionParams(
-//                            text: "Sunny",
-//                            icon: "https://cdn.weatherapi.com/weather/64x64/day/113.png"
-//                        )
-//                    )
-//                ),
-//                Forecast(
-//                    date: "2023-07-02",
-//                    day: Day(
-//                        avgtemp_c: 10,
-//                        avgtemp_f: 50,
-//                        condition: conditionParams(
-//                            text: "Sunny",
-//                            icon: "https://cdn.weatherapi.com/weather/64x64/day/113.png"
-//                        )
-//                    )
-//                ),
-//                Forecast(
-//                    date: "2023-07-03",
-//                    day: Day(
-//                        avgtemp_c: 10,
-//                        avgtemp_f: 50,
-//                        condition: conditionParams(
-//                            text: "Sunny",
-//                            icon: "https://cdn.weatherapi.com/weather/64x64/day/113.png"
-//                        )
-//                    )
-//                ),
-//            ]
-//        )
-//    )
-
+    //    @State var foreCast: ForecastResponse = ForecastResponse(
+    //        location: LocationParams(
+    //            name: "London",
+    //            localtime: "2023-07-01"
+    //        ),
+    //        forecast: ForecastParams(
+    //            forecastday: [
+    //                Forecast(
+    //                    date: "2023-07-01",
+    //                    day: Day(
+    //                        avgtemp_c: 10,
+    //                        avgtemp_f: 50,
+    //                        condition: conditionParams(
+    //                            text: "Sunny",
+    //                            icon: "https://cdn.weatherapi.com/weather/64x64/day/113.png"
+    //                        )
+    //                    )
+    //                ),
+    //                Forecast(
+    //                    date: "2023-07-02",
+    //                    day: Day(
+    //                        avgtemp_c: 10,
+    //                        avgtemp_f: 50,
+    //                        condition: conditionParams(
+    //                            text: "Sunny",
+    //                            icon: "https://cdn.weatherapi.com/weather/64x64/day/113.png"
+    //                        )
+    //                    )
+    //                ),
+    //                Forecast(
+    //                    date: "2023-07-03",
+    //                    day: Day(
+    //                        avgtemp_c: 10,
+    //                        avgtemp_f: 50,
+    //                        condition: conditionParams(
+    //                            text: "Sunny",
+    //                            icon: "https://cdn.weatherapi.com/weather/64x64/day/113.png"
+    //                        )
+    //                    )
+    //                ),
+    //            ]
+    //        )
+    //    )
+    
     func fetchForecast() async {
         do {
             let result = try await WeatherService().getForecast(location: "london-city-of-london-greater-london-united-kingdom")
@@ -56,51 +56,51 @@ struct ForecastPage: View {
             print(error)
         }
     }
-
+    
     var body: some View {
         NavigationView {
-
+            
             if foreCast != nil {
                 
-            
-            Text("\(foreCast.location.name)")
-        Text("\(foreCast.location.localtime)")
-            AsyncImage(url: URL(string: "\(foreCast.forecast.forecastday[0].day.condition.icon)")!) { image in
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-        } placeholder: {
-            ProgressView()
-        }
-            Text("\(foreCast.forecast.forecastday[0].day.condition.text)")
-        List(foreCast.forecast.forecastday) { day in
-            ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color.white)
-                    .shadow(radius: 5)
-                HStack {
-                    Text("\(day.date)")
-                    Text("\(day.day.avgtemp_c)")
-                    Text("\(day.day.condition.icon)")
-                    Text("\(day.day.condition.text)")
+                
+                Text("\(foreCast!.location.name)")
+                Text("\(foreCast!.location.localtime)")
+                AsyncImage(url: URL(string: "\(foreCast!.forecast.forecastday[0].day.condition.icon)")!) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                } placeholder: {
+                    ProgressView()
                 }
-            }
-
-        }
+                Text("\(foreCast!.forecast.forecastday[0].day.condition.text)")
+                List(foreCast!.forecast.forecastday) { day in
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.white)
+                            .shadow(radius: 5)
+                        HStack {
+                            Text("\(day.date)")
+                            Text("\(day.day.avgtemp_c)")
+                            Text("\(day.day.condition.icon)")
+                            Text("\(day.day.condition.text)")
+                        }
+                    }
+                    
+                }
             } else {
                 ProgressView()
             }
-
-        .navigationTitle("Forecast")
-        .task {
-            await fetchForecast()
-        }
+            
+                .navigationTitle("Forecast")
+                .task {
+                    await fetchForecast()
+                }
         }
     }
 }
 
 struct ForecastPage_Previews: PreviewProvider {
-  @State static var url=""
+    @State static var url=""
     static var previews: some View {
         ForecastPage(location:url)
     }
